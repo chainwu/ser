04571d6b1a6c6cdde09b1a58ee9e2607bb48e7c3
Chain-Wu Lee <lee.chain-wu@inventec.com>
2018-11-14 22:55:58 +0800

    Add plot and label functionality

--- code/ser-classifier.py
+++ code/ser-classifier.py
@@ -5,12 +5,41 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.axis([0, 10, 0, 1], 'off')
+def plot_label():
+    plt.text(0, -3, 'Neutral', size=10, ha="center", va="center",
+    	     bbox=dict(boxstyle="round",
+        	       ec=(1., 0.5, 0.5),
+                       fc=(1., 0.8, 0.8),))
+    plt.text(0, 7, 'Anger', size=10, ha="center", va="center",
+    	     bbox=dict(boxstyle="round",
+        	       ec=(1., 0.5, 0.5),
+                       fc=(1., 0.8, 0.8),))
+    plt.text(10, -3, 'Happiness', size=10, ha="center", va="center",
+    	     bbox=dict(boxstyle="round",
+        	       ec=(1., 0.5, 0.5),
+                       fc=(1., 0.8, 0.8),))
+    plt.text(-10, -3, 'Sarcasm', size=10, ha="center", va="center",
+    	     bbox=dict(boxstyle="round",
+        	       ec=(1., 0.5, 0.5),
+                       fc=(1., 0.8, 0.8),))
+    plt.text(0, -13, 'Surprise', size=10, ha="center", va="center",
+    	     bbox=dict(boxstyle="round",
+        	       ec=(1., 0.5, 0.5),
+                       fc=(1., 0.8, 0.8),))
 
-circle1 = plt.Circle((0, 0), 1, color='r')
-circle2 = plt.Circle((0, 10), 1, color='blue')
-circle3 = plt.Circle((10, 0), 1, color='g', clip_on=False)
-circle4 = plt.Circle((-10, 0), 1, color='g', clip_on=False)
-circle5 = plt.Circle((0, -10), 1, color='g', clip_on=False)
+
+plt.axis([-15, 15, -15, 15], 'off')
+
+x = [10, -10, 0, 0, 0]
+y = [ 0,   0, 0, 10, -10]
+
+plt.plot(x, y, 'o', color='1', markeredgecolor='r', markersize=20)
+plot_label()
+
+#circle1 = plt.Circle((0, 0), 1, color='r')
+#circle2 = plt.Circle((0, 10), 1, color='blue')
+#circle3 = plt.Circle((10, 0), 1, color='g', clip_on=False)
+#circle4 = plt.Circle((-10, 0), 1, color='g', clip_on=False)
+#circle5 = plt.Circle((0, -10), 1, color='g', clip_on=False)
 plt.show()
 plt.pause(5)
