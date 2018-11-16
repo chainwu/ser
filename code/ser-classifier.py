79da6531c3322d3f112f2092bbf72b86fe0abef0
Chain-Wu Lee <lee.chain-wu@inventec.com>
2018-11-16 19:00:47 +0800

    Modulize classifier

--- code/ser-classifier.py
+++ code/ser-classifier.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-#import keras
+import keras
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+from keras.models import load_model
+
+SER_MODEL = 'ser-1.h5'
 
 def plot_label():
     plt.text(0, -3, 'Neutral', size=10, ha="center", va="center",
@@ -28,18 +32,37 @@ def plot_label():
                        fc=(1., 0.8, 0.8),))
 
 
-plt.axis([-15, 15, -15, 15], 'off')
+def plot_baseplane():
+    plt.axis([-15, 15, -15, 15], 'off')
 
-x = [10, -10, 0, 0, 0]
-y = [ 0,   0, 0, 10, -10]
+    x = [10, -10, 0, 0, 0]
+    y = [ 0,   0, 0, 10, -10]
 
-plt.plot(x, y, 'o', color='1', markeredgecolor='r', markersize=20)
-plot_label()
+    plt.plot(x, y, 'o', color='1', markeredgecolor='r', markersize=20)
+    plot_label()
 
-#circle1 = plt.Circle((0, 0), 1, color='r')
-#circle2 = plt.Circle((0, 10), 1, color='blue')
-#circle3 = plt.Circle((10, 0), 1, color='g', clip_on=False)
-#circle4 = plt.Circle((-10, 0), 1, color='g', clip_on=False)
-#circle5 = plt.Circle((0, -10), 1, color='g', clip_on=False)
-plt.show()
-plt.pause(5)
+    plt.show()
+
+def load_cnn():
+    model = load_model(SER_MODEL)
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+def read_input():
+
+def cnn_classify():
+    model.predict()
+    
+def main():
+    try:
+        plot_baseplane()
+        plot_label()
+        load_cnn()
+        read_input()
+        cnn_classify()
+        
+    except:
+        print("Exception!")
+
+if __name__ == '__main__':
+    main()
